a9c8e4f434a2981858f5e8453dd87752fbac135c
Bogibek_Matyaqubov <uzroot.pdp@gmail.com>
2022-03-28 16:25:08 +0500

    second

--- src/main/java/dev/matyaqubov/simple/SimpleApplication.java
+++ src/main/java/dev/matyaqubov/simple/SimpleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SimpleApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SimpleApplication.class, args);
 	}
 
